19ab4f31e6cedd5f4d90eb40ee865cfdf5f9c483
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-09 00:57:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes={
 
 function updateObjectWithKeyAndValue(object, key, value){
   var recipes={prop:1}
-  return Object.assign({}, obj,{prop2:2})
+  return Object.assign({}, recipes,{prop2:2})
 }
 
 
