0cce8b317925a45b0d7a29b7a6fe70b1ebc6206c
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-09 00:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-  recipes.prop2='2'
-  return recipes
+  return recipes.prop2='2'
+  
 
 }
 
