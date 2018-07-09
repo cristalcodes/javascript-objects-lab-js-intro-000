bca68ab025d081794d21e56da29119f124a1f04f
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-09 00:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', '2')
+  destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', '2')
 }
 
 
